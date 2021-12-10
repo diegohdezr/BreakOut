d24039ad16174d0b0ca6344b8314966f2676b45d
Diego Hernandez <diego.hdez@hotmail.com>
2021-12-10 02:03:44 -0600

    Modulo 10
    Configuracion de camara y creacion de assets basicos

---
+++ New Unity Project/Assets/_Scripts/Bloque_Piedra.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Piedra : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 5;
+    }
+
+}
