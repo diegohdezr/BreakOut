877ced33b434e770a89f6fe6e140203e105db73f
Diego Hernandez <diego.hdez@hotmail.com>
2021-12-30 00:30:24 -0600

    Modulo12
    -se modificaron los scripts de bola y jugador para hacer uso del input manager

--- New Unity Project/Assets/_Scripts/Bola.cs
+++ New Unity Project/Assets/_Scripts/Bola.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+
+    bool isGameStarted;
+    [SerializeField] public float velocidadBola = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
+        isGameStarted = false;
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            if (!isGameStarted) 
+            {
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = velocidadBola * Vector3.up;
+            }
+        }
     }
 }
--- New Unity Project/Assets/_Scripts/Jugador.cs
+++ New Unity Project/Assets/_Scripts/Jugador.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float VelocidadPaddle = 2f;
+
+    Transform transform;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //mousePos2D = Input.mousePosition;
+        //mousePos2D.z = -Camera.main.transform.position.z;
+        //mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        
+        
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            transform.Translate(Vector3.down * VelocidadPaddle * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            transform.Translate(Vector3.up * VelocidadPaddle * Time.deltaTime);
+        }
+
+        Vector3 pos = transform.position;
+        //pos.x = mousePos3D.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX) 
+        {
+            pos.x = limiteX;
+        }
+
+        transform.position = pos;
         
     }
 }
